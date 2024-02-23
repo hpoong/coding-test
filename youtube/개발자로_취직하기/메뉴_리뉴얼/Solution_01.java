4849ce6d5efa9b968215bbb020cde77043b98c5c
hpoong <hopoong23@gmail.com>
2024-02-23 18:59:58 +0900

    # 수정사항 - 메뉴 리뉴얼 풀이 완료

--- youtube/개발자로_취직하기/메뉴_리뉴얼/Solution_01.java
+++ youtube/개발자로_취직하기/메뉴_리뉴얼/Solution_01.java
@@ -1,38 +1,59 @@
 package 개발자로_취직하기.메뉴_리뉴얼;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution_01 {
 
-//    List<String> anws
+
+    List<String> answerList = new ArrayList<>();
+    Map<String, Integer> hashMap = new HashMap<>();
 
     public String[] solution(String[] orders, int[] course) {
 
-        // 각 order 정렬
-        for(int i=0; i<orders.length; i++) {
-            char[] chars = orders[i].toCharArray();
-            Arrays.sort(chars);
-            orders[i] = String.valueOf(chars);
+        // 1. 각 Order 정렬
+        for (int i = 0; i < orders.length; i++) {
+            char[] arr = orders[i].toCharArray();
+            Arrays.sort(arr);
+            orders[i] = String.valueOf(arr);
         }
 
-        // 각 order기준으로 course leng 만큼의 조합 만들기
-        for(int courseLeng: course) {
-            for(String order: orders) {
-                combination("", order, courseLeng);
+        // 2. 각 order를 기준으로 courseLength 만큼의 조합 만들기
+        for (int courseLength : course) {
+            for (String order : orders)
+                combination("", order, courseLength);
+
+            // 3. 가장 많은 조합 answer에 저장
+            if (!hashMap.isEmpty()) {
+                List<Integer> countList = new ArrayList<>(hashMap.values());
+                int max = Collections.max(countList);
+
+                if (max > 1)
+                    for (String key : hashMap.keySet())
+                        if (hashMap.get(key) == max)
+                            answerList.add(key);
+                hashMap.clear();
             }
         }
 
-        // 가장 많은 조합을 answer에 저장한다.
+        Collections.sort(answerList);
+        String[] answer = new String[answerList.size()];
+        for (int i = 0; i < answer.length; i++)
+            answer[i] = answerList.get(i);
 
-
-        String[] answer = {};
         return answer;
     }
 
-    private void combination(String str, String order, int courseLeng) {
+    public void combination(String order, String others, int count) {
+        // 탈출 조건 : count == 0
+        if (count == 0) {
+            hashMap.put(order, hashMap.getOrDefault(order, 0) + 1);
+            return;
+        }
 
+        // 0부터 length까지 조합
+        for (int i = 0; i < others.length(); i++)
+            combination(order + others.charAt(i), others.substring(i + 1), count - 1);
     }
-
 
     public static void main(String[] args) {
         String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
